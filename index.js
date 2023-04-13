f72930356ed51a45e5d4b352a1529a66cdd1ab60
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-04-13 18:28:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// завдання 4
+let a = 5;
+a +=7;
+console.log(a);
+let b = 10;
+b -= 6;
+console.log(b);
+let c = 15;
+c *= 45;
+console.log(c);
+let d = 20;
+d /= 2;
+console.log(d);
+// завдання 2
+const passed = prompt("скильки вам рокив?"); 
+const isAdute = passed > 18 && passed === 18;
+console.log(isAdute);
+// завдання 1
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+const totalPrice = (pricePerItem * orderedQuantity );
+console.log(totalPrice);
+// завдання 3
+const SAVED_PASSWORD = 1234567;
+const writePassword = promt("ведить пароль")
+const isMath = SAVED_PASSWORD === writePassword;
+console.log(isMath);
